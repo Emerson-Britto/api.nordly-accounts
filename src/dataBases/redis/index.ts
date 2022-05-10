23d2de781efc51acccf187944cbc937a14801d23
Emerson-Britto <emersonbritto987@gmail.com>
2022-05-10 05:59:39 -0300

    cleaning test code

--- src/dataBases/redis/index.ts
+++ src/dataBases/redis/index.ts
@@ -18,37 +18,3 @@ class Redis {
 }
 
 export default new Redis();
-
-
-// const redisDB = async() => {
-//   return new Promise(async(resolve, reject): Promise<RedisClientType> => {
-//     const client:RedisClientType = createClient<RedisClientOptions>({
-//       url: 'redis-16973.c256.us-east-1-2.ec2.cloud.redislabs.com:16973',
-//       password: '1iXzcGhWUqSRnyvif0OTaJpDIJgGMYtt'
-//     });
-
-//     client.on('error', reject);
-
-//     await client.connect();
-//     resolve(client);
-//   })
-// }
-
-// export default redisDB();
-
-
-// import { createClient, RedisClientOptions, RedisClientType } from "redis";
-
-// const factory = (options: RedisClientOptions<any, any>): RedisClientType => {
-//   return createClient(options);
-// };
-
-// import { createClient } from "redis";
-// type RedisClientType = ReturnType<typeof createClient>;
-// type RedisClientOptions = Parameters<typeof createClient>[0];
-
-// const factory = (options: RedisClientOptions): RedisClientType => {
-//   return createClient(options);
-// };
-
-// const client: RedisClientType = factory({});
